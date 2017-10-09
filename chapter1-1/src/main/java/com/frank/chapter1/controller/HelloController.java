a68c4ce5bf5ce80d4715e4a5f42154175a6ba5e3
简伟林 <jianwl999@gmail.com>
2017-10-09 22:19:48 +0800

    构建基本项目，引入web模块，完成一个简单的Restful API

---
+++ chapter1-1/src/main/java/com/frank/chapter1/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.frank.chapter1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jianweilin on 2017/10/9.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String index(){
+        return "Hello World!";
+    }
+}
