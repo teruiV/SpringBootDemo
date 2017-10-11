f52b4d58276418fe282cbb5e97698cebaf5c538b
简伟林 <jianwl999@gmail.com>
2017-10-11 17:49:47 +0800

    使用freemarker模板引擎

---
+++ chapter3-3/src/main/java/com/frank/chapter33/Chapter33Application.java
@@ -0,0 +1,12 @@
+package com.frank.chapter33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter33Application.class, args);
+	}
+}
---
+++ chapter3-3/src/main/resources/templates/index.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Title</title>
+</head>
+<body>
+FreeMarker模板引擎
+<h1>${host}</h1>
+</body>
+</html>
