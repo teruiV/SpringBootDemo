2aa72103c68a19a17e6c86c94dcfc29881831fb0
简伟林 <jianwl999@gmail.com>
2017-10-28 15:02:53 +0800

    使用NoSQL数据库（二）：MongoDB

--- chapter4-6/src/test/java/com/frank/chapter46/Chapter46ApplicationTests.java
+++ chapter4-6/src/test/java/com/frank/chapter46/Chapter46ApplicationTests.java
@@ -24,22 +24,11 @@ public class Chapter46ApplicationTests {
 	
 
 	@Test
-	public void test(){
+	public void test_001(){
 		// 创建三个User,并验证User总数
 		userRepository.save(new User(1L,"张三",30));
 		userRepository.save(new User(2L,"王武",40));
 		userRepository.save(new User(3L,"招商",50));
 		Assert.assertEquals(3,userRepository.findAll().size());
-
-		// 删除一个User,再验证User总数
-		User u = userRepository.findOne(1L);
-		userRepository.delete(u);
-		Assert.assertEquals(2,userRepository.findAll().size());
-
-		// 删除一个User，在验证User总数
-		u = userRepository.findByUsername("张三");
-		userRepository.delete(u);
-		Assert.assertEquals(1,userRepository.findAll().size());
-
 	}
 }
